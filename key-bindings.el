29d3584dc2104347a776479fa1ca08265e1aa4fd
Matthias Nuessler <m.nuessler@web.de>
2012-07-03 00:39:29 +0200

    Move key-bindings from .emacs to a separate file.

---
+++ key-bindings.el
@@ -0,0 +1,39 @@
+;; Keyboard shortcut for toggling autofill mode
+;;(global-set-key (kbd "C-c q") 'auto-fill-mode)
+
+;; Sets `C-c d` to `M-x kill-whole-line`
+(global-set-key "\C-cd" 'kill-whole-line)
+
+;; org-mode
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
+;; Replace current word by synonym using a thesaurus
+(define-key global-map (kbd "C-x t") 'thesaurus-choose-synonym-and-replace)
+
+;; expand region
+(global-set-key (kbd "C-=") 'er/expand-region)
+
+;; replace the original string-rectangle binding
+;; by the inline version from mark-multiple
+(global-set-key (kbd "C-x r t") 'inline-string-rectangle)
+
+;; bindings for mark-more-like-this
+(global-set-key (kbd "C-<") 'mark-previous-like-this)
+(global-set-key (kbd "C->") 'mark-next-like-this)
+(global-set-key (kbd "C-M-m") 'mark-more-like-this) ; like the other two, but takes an argument (negative is previous)
+(global-set-key (kbd "C-*") 'mark-all-like-this)
+
+;; ace-jump-mode
+(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+;; If you also use viper mode :
+;;(define-key viper-vi-global-user-map (kbd "SPC") 'ace-jump-mode)
+;; If you use evil
+;;(define-key evil-normal-state-map (kbd "SPC") 'ace-jump-mode)
+
+;; Magit
+(global-set-key (kbd "C-x m") 'magit-status)
+
+(provide 'key-bindings)
+
