5db3cd8cbd15a26676a0e97b81c52a592ac07675
Matthias Nuessler <m.nuessler@web.de>
2012-07-03 23:29:48 +0200

    Add submodule smex (smart M-x enhancement).

---
+++ init.el
@@ -0,0 +1,4 @@
+;; Initialize smex, a smart Emacs enhancement
+(require 'smex)
+(smex-initialize)
+
--- key-bindings.el
+++ key-bindings.el
@@ -35,5 +35,11 @@
 ;; Magit
 (global-set-key (kbd "C-x m") 'magit-status)
 
+;; Smex
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; This is your old M-x.
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
 (provide 'key-bindings)
 
