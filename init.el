ed972e362729907426eb12750a6e3befcab507c7
Matthias Nuessler <m.nuessler@web.de>
2012-07-05 20:10:36 +0200

    Reuse directory buffers in dired.

--- init.el
+++ init.el
@@ -7,3 +7,7 @@
 (require 'smex)
 (smex-initialize)
 
+;; Reuse directory buffers in dired (provided by dired-plus)
+;; see http://emacswiki.org/emacs/DiredReuseDirectoryBuffer
+(toggle-diredp-find-file-reuse-dir 1)
+
