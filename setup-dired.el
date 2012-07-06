297142df62a6181bea18cbed35f47b9426327a65
Matthias Nuessler <m.nuessler@web.de>
2012-07-07 00:13:32 +0200

    Set up dired-x.
    Enable omitting of 'uninteresting' files.

--- setup-dired.el
+++ setup-dired.el
@@ -1,5 +1,19 @@
 ;; Set to non-nil to enable recursive deletion of directories
 (setq dired-recursive-deletes nil)
 
+;; Set up dired-x
+(add-hook 'dired-load-hook
+	  (lambda ()
+	    (load "dired-x")
+	    ;; Set dired-x global variables here.  For example:
+	    ;; (setq dired-guess-shell-gnutar "gtar")
+	    ;; (setq dired-x-hands-off-my-keys nil)
+	    ))
+(add-hook 'dired-mode-hook
+	  (lambda ()
+	    ;; Set dired-x buffer-local variables here.
+	    (dired-omit-mode 1)
+	    ))
+
 (provide 'setup-dired)
 
