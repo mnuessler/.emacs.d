50b792e183433b7f6ed79fcb840415f56fcc9ed8
Matthias Nuessler <m.nuessler@web.de>
2012-07-07 15:57:45 +0200

    Add LaTeX preferences for Mac to Custom.

--- custom.el
+++ custom.el
@@ -9,6 +9,12 @@
  '(erc-nick "mnuessler")
  '(erc-server "irc.iscout.local")
  '(erc-user-full-name "Matthias Nuessler"))
+ '(LaTeX-command "/usr/texbin/pdflatex -synctex=1")
+ '(inhibit-startup-screen t)
+ '(ispell-program-name "/sw/bin/ispell")
+ '(latex-run-command "/usr/texbin/pdflatex -synctex=1")
+ '(save-place t nil (saveplace))
+ '(show-paren-mode t))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
