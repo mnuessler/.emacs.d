7fd320dcc5293dcf912d73adeba56c187d35a3c6
Matthias Nuessler <m.nuessler@web.de>
2012-07-12 21:01:33 +0200

    Configure highlight color.

--- custom.el
+++ custom.el
@@ -27,5 +27,6 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(hl-line ((t (:inherit highlight :background "#222"))))
- '(col-highlight ((t (:inherit highlight :background "#222")))))
+ '(col-highlight ((t (:inherit highlight :background "#222"))))
+ '(highlight ((t (:background "#222"))))
+ '(hl-line ((t (:inherit highlight :background "#222")))))
