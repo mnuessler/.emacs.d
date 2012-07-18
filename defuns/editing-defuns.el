021af8f4971f9b2c62f69910e0ff8e3fafa35571
Matthias Nuessler <m.nuessler@web.de>
2012-07-18 23:43:50 +0200

    Add function to convert CamelCase to underscore and vice versa.
    The function cycles between 4 different word delimiters: CamelCase, underscore, dashes and colons. Example: CamelCase -> camel_case -> camel-case -> camel::case. Taken from EmacsWiki.

--- defuns/editing-defuns.el
+++ defuns/editing-defuns.el
@@ -24,3 +24,33 @@
 		   (forward-word arg)
 		   (point)))
       (activate-mark))))
+
+
+;; cycle conversion between CamelCase under_score etc. styles
+;; source: http://www.emacswiki.org/CamelCase
+(defun split-name (s)
+  (split-string
+   (let ((case-fold-search nil))
+     (downcase
+      (replace-regexp-in-string "\\([a-z]\\)\\([A-Z]\\)" "\\1 \\2" s)))
+   "[^A-Za-z0-9]+"))
+
+(defun camelcase  (s) (mapconcat 'capitalize (split-name s) ""))
+(defun underscore (s) (mapconcat 'downcase   (split-name s) "_"))
+(defun dasherize  (s) (mapconcat 'downcase   (split-name s) "-"))
+(defun colonize   (s) (mapconcat 'capitalize (split-name s) "::"))
+
+(defun camelscore (s)
+  (cond ((string-match-p "\:" s) (camelcase  s))
+	((string-match-p "-"  s) (colonize   s))
+	((string-match-p "_"  s) (dasherize  s))
+	(t                       (underscore s))))
+
+(defun camelscore-word-at-point ()
+  (interactive)
+  (let* ((case-fold-search nil)
+	 (beg (and (skip-chars-backward "[:alnum:]:_-") (point)))
+	 (end (and (skip-chars-forward  "[:alnum:]:_-") (point)))
+	 (txt (buffer-substring beg end))
+	 (cml (camelscore txt)) )
+    (if cml (progn (delete-region beg end) (insert cml))) ))
