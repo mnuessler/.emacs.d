b8b7a42e2fc9048fc1ef609576ff9fdeb7a464ab
Matthias Nuessler <m.nuessler@web.de>
2012-08-05 00:45:33 +0200

    Enable camelCase-mode for python-mode.

--- setup-python-mode.el
+++ setup-python-mode.el
@@ -3,5 +3,6 @@
 (setq lambda-symbol (string (make-char 'greek-iso8859-7 107)))
 (add-hook 'python-mode-hook #'lambda-mode 1)
 (add-hook 'python-mode-hook #'highlight-indentation-on)
+(add-hook 'python-mode-hook #'camelCase-mode 1)
 
 (provide 'setup-python-mode)
