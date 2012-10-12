3767f5484195907c55f66c8daf508d0431f5227c
Matthias Nuessler <m.nuessler@web.de>
2012-10-13 00:49:37 +0200

    Replace submodule gitconfig-mode with git-modes.
    Gitconfig-mode has been merged into git-modes.

--- init.el
+++ init.el
@@ -80,7 +80,7 @@
 (setq ac-auto-start nil)
 (require 'bookmark+)
 (require 'gitconfig-mode)
-(require 'scala-mode-auto)
+(require 'scala-mode)
 
 ;; drag stuff
 ;; turn it on globally, except listed modes
