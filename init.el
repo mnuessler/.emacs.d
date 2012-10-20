146ada0f43cdd5bfbcb835c2b0c4d11f27aae376
Matthias Nuessler <m.nuessler@web.de>
2012-10-20 23:08:50 +0200

    Add gist.el plus dependencies.
    Access gists on github via remote API, add gist from buffer or region etc.

--- init.el
+++ init.el
@@ -256,3 +256,6 @@
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
 			 ("marmalade" . "http://marmalade-repo.org/packages/")
 			 ("melpa" . "http://melpa.milkbox.net/packages/")))
+
+;; Access gists on github
+(require 'gist)
