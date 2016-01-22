8c42ff7c821cb89451ab258a594f7c3afbbd0f1f
Matthias Nuessler <m.nuessler@web.de>
2016-01-22 16:21:40 +0100

    Add key bindings for swiper

--- key-bindings.el
+++ key-bindings.el
@@ -98,4 +98,12 @@
 (global-set-key [remap move-beginning-of-line]
                 'smarter-move-beginning-of-line)
 
+(when (require 'swiper nil t)
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t)
+  (global-set-key "\C-s" 'swiper)
+  (global-set-key "\C-r" 'swiper)
+  (global-set-key (kbd "C-c C-r") 'ivy-resume)
+  (global-set-key [f6] 'ivy-resume))
+
 (provide 'key-bindings)
